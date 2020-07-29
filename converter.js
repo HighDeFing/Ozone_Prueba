78469897e0bc2d2ad31f94a24dab8d94e7554da8
Heider Delgado <heider2112@gmail.com>
2020-07-29 18:56:12 -0400

    converter of node for obj, still need works of parsing

---
+++ converter.js
@@ -0,0 +1,9 @@
+const obj2gltf = require('obj2gltf');
+const fs = require('fs');
+const options = {
+    binary : true
+}
+obj2gltf('./test_files/mono.obj', options)
+    .then(function(glb) {
+        fs.writeFileSync('./test_files/model1.glb', glb);
+    });
